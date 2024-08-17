5f241d490166f63b8eeddf3c999e13fe379c4db7
Diego Cáceres Cardoza <diegokaiser@gmail.com>
2024-08-17 12:52:16 +0200

    init

---
+++ app/libs/utils/index.js
@@ -0,0 +1,4 @@
+export { default as capitalize } from './capitalize'
+export { default as firebase } from './firebase'
+export { default as isBrowser } from './isBrowser'
+export { default as timeFormat } from './timeFormat'
