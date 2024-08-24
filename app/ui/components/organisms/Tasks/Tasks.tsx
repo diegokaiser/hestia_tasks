f70f86d97cb0283183f8cbfc3e801c3cf0d732fe
Diego Cáceres Cardoza <diegokaiser@gmail.com>
2024-08-24 23:26:38 +0200

    botones para editar y eliminar

--- app/ui/components/organisms/Tasks/Tasks.tsx
+++ app/ui/components/organisms/Tasks/Tasks.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import { useEffect, useState } from "react"
-import { FaCheck, FaTimes } from "react-icons/fa";
+import { useRouter } from 'next/navigation';
+import { FaCheck, FaPencilAlt, FaRegTrashAlt, FaTimes } from "react-icons/fa";
 import { FaExclamation } from "react-icons/fa6";
 import Apis from "@/app/libs/apis";
 import { LoadingScreen } from "@/app/ui/components/molecules";
@@ -22,6 +23,22 @@ interface Props {
 const Tasks = ({userUid, listUid}: Props) => {
   const [loading, setLoading] = useState<boolean>(false)
   const [dataTasks, setDataTasks] = useState<Task[]>([])
+  const router = useRouter()
+
+  const handleDeleteTask = async (userUid: string, listUid: string, taskUid: string) => {
+    setLoading(true)
+    try {
+      await Apis.tasks.DeleteTask(userUid, listUid, taskUid)
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const gotoEdit = (taskUid: string) => {
+    router.push(`/tasks/${taskUid}`)
+  }
 
   const handleUpdateTask = async (taskUid: string, currentCompleted: boolean) => {
     try {
@@ -57,34 +74,44 @@ const Tasks = ({userUid, listUid}: Props) => {
 
   return (
     <>
-      <div className="hs__tasks flex flex-col gap-3">
+      <div className="hs__tasks flex flex-col gap-3 overflow-hidden">
         {dataTasks.length > 0 ? (
           <>
             {dataTasks.map(task => (
               <div 
                 key={task.uid}
-                className={`task cursor-pointer flex gap-3 items-center justify-start completed-${task.completed} relative`}
+                className={`task overflow-x-auto scroll-smooth relative scrollbar-hide`}
                 onClick={() => handleUpdateTask(task.uid, task.completed)}
               >
-                <div className={`task__completed flex items-center justify-center`}>
-                  {task.completed ? (
-                    <>
-                      <FaCheck />
-                    </>
-                  ) : (
-                    <>
-                      <FaTimes />
-                    </>
+                <div className={`task__edit absolute cursor-pointer flex flex-col gap-1 items-center justify-center`} style={{ right: '-81px' }}>
+                  <FaPencilAlt />
+                  <p className="text-xs">Edit</p>
+                </div>
+                <div className={`task__content completed-${task.completed} cursor-pointer flex gap-3 items-center justify-start relative`}>
+                  <div className={`task__completed flex items-center justify-center`}>
+                    {task.completed ? (
+                      <>
+                        <FaCheck />
+                      </>
+                    ) : (
+                      <>
+                        <FaTimes />
+                      </>
+                    )}
+                  </div>
+                  <div className="font-semibold">
+                    {task.name}
+                  </div>
+                  {task.priority == 'high' && (
+                    <div className="task__priority absolute flex items-center justify-center">
+                      <FaExclamation />
+                    </div>
                   )}
                 </div>
-                <div className="font-semibold">
-                  {task.name}
+                <div className={`task__delete absolute cursor-pointer flex flex-col gap-1 items-center justify-center `} style={{ right: '-154px' }}>
+                  <FaRegTrashAlt />
+                  <p className="text-xs">Delete</p>
                 </div>
-                {task.priority == 'high' && (
-                  <div className="task__priority absolute flex items-center justify-center">
-                    <FaExclamation />
-                  </div>
-                )}
               </div>
             ))}
           </>
